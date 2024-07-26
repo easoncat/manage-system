49af268d289d084a79fd707a86323c5ce8458094
Gong <YiShen.Gong@siemens.com>
2024-07-26 17:45:01 +0800

    add redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 import './index.css'
 import 'antd/dist/reset.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
+    
 );
